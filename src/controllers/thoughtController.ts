4d644a9eb3e1f60a50c2f2ec8ae0c006df7658da
Deepthi Sandeep <deepththampi@hotmail.com>
2024-11-09 18:14:18 -0600

    updated thought controller - 003 and updated user controller

--- src/controllers/thoughtController.ts
+++ src/controllers/thoughtController.ts
@@ -53,3 +53,95 @@ export const getThoughtById = async (req: Request, res: Response) => {
         res.status(400).json({ message: e.message });
     }
   }
+
+  /**
+ * PUT Thought based on id /thoughts/:id
+ * @param string id
+ * @param object updated data
+ * @returns the updated Thought object
+ */
+export const updateThought = async (req: Request, res: Response) => {
+    try {
+      const thought = await Thought.findOneAndUpdate(
+        { _id: req.params.id },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+  
+      if (!thought) {
+        res.status(404).json({ message: 'No thought with this id!' });
+      } else {
+        res.json(thought);
+      }
+    } catch (error: any) {
+      res.status(400).json({ message: error.message });
+    }
+  };
+
+  /**
+ * DELETE Thought based on id /thoughts/:id
+ * @param string id
+ * @returns string
+ */
+export const deleteThought = async (req: Request, res: Response) => {
+    try {
+      const thought = await Thought.findOneAndDelete({ _id: req.params.id });
+  
+      if (!thought) {
+        res.status(404).json({ message: 'No thought with that ID' });
+      } else {
+        await User.updateMany({ thoughts: thought._id }, { $pull: { thoughts: thought._id } });
+        res.json({ message: 'Thought deleted!' });
+      }
+    } catch (error: any) {
+      res.status(500).json({ message: error.message });
+    }
+  };
+  
+
+/**
+ * POST Reaction to Thought /thoughts/:thoughtId/reactions
+ * @param string thoughtId
+ * @param object reaction
+ * @returns the updated Thought object
+ */
+export const addReaction = async (req: Request, res: Response) => {
+    try {
+      const thought = await Thought.findByIdAndUpdate(
+        req.params.thoughtId,
+        { $push: { reactions: req.body } },
+        { new: true }
+      );
+  
+      if (!thought) {
+        res.status(404).json({ message: 'Thought not found' });
+      } else {
+        res.json(thought);
+      }
+    } catch (error: any) {
+      res.status(500).json({ message: error.message });
+    }
+  };
+
+  /**
+ * DELETE Reaction from Thought /thoughts/:thoughtId/reactions/:reactionId
+ * @param string thoughtId, reactionId
+ * @returns the updated Thought object
+ */
+export const removeReaction = async (req: Request, res: Response) => {
+    try {
+      const thought = await Thought.findByIdAndUpdate(
+        req.params.thoughtId,
+        { $pull: { reactions: { reactionId: req.params.reactionId } } },
+        { new: true }
+      );
+  
+      if (!thought) {
+        res.status(404).json({ message: 'Thought not found' });
+      } else {
+        res.json(thought);
+      }
+    } catch (error: any) {
+      res.status(500).json({ message: error.message });
+    }
+  };
---
+++ src/controllers/userControntroller.ts
@@ -0,0 +1,139 @@
+import { Request, Response } from 'express';
+import User from '../models/User';
+import Thought from '../models/Thought';
+
+/**
+ * GET All Users /users
+ * @returns an array of User objects
+ */
+export const getAllUsers = async (_req: Request, res: Response) => {
+  try {
+    const users = await User.find().populate('thoughts').populate('friends');
+    res.json(users);
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+/**
+ * GET User based on id /users/:id
+ * @param string id
+ * @returns a single User object
+ */
+export const getUserById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const user = await User.findById(id).populate('thoughts').populate('friends');
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+/**
+ * POST User /users
+ * @param object username, email
+ * @returns the created User object
+ */
+export const createUser = async (req: Request, res: Response) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json(user);
+  } catch (error: any) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+/**
+ * PUT User based on id /users/:id
+ * @param string id
+ * @param object updated data
+ * @returns the updated User object
+ */
+export const updateUser = async (req: Request, res: Response) => {
+  try {
+    const user = await User.findOneAndUpdate(
+      { _id: req.params.id },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    );
+
+    if (!user) {
+      res.status(404).json({ message: 'No user with this id!' });
+    } else {
+      res.json(user);
+    }
+  } catch (error: any) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+/**
+ * DELETE User based on id /users/:id
+ * @param string id
+ * @returns string
+ */
+export const deleteUser = async (req: Request, res: Response) => {
+  try {
+    const user = await User.findOneAndDelete({ _id: req.params.id });
+
+    if (!user) {
+      res.status(404).json({ message: 'No user with that ID' });
+    } else {
+      await Thought.deleteMany({ _id: { $in: user.thoughts } });
+      res.json({ message: 'User and associated thoughts deleted!' });
+    }
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+/**
+ * POST Friend to User /users/:userId/friends/:friendId
+ * @param string userId, friendId
+ * @returns the updated User object
+ */
+export const addFriend = async (req: Request, res: Response) => {
+  try {
+    const user = await User.findByIdAndUpdate(
+      req.params.userId,
+      { $addToSet: { friends: req.params.friendId } },
+      { new: true }
+    );
+
+    if (!user) {
+      res.status(404).json({ message: 'User not found' });
+    } else {
+      res.json(user);
+    }
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+/**
+ * DELETE Friend from User /users/:userId/friends/:friendId
+ * @param string userId, friendId
+ * @returns the updated User object
+ */
+export const removeFriend = async (req: Request, res: Response) => {
+  try {
+    const user = await User.findByIdAndUpdate(
+      req.params.userId,
+      { $pull: { friends: req.params.friendId } },
+      { new: true }
+    );
+
+    if (!user) {
+      res.status(404).json({ message: 'User not found' });
+    } else {
+      res.json(user);
+    }
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+};
