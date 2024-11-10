7b26629ef20a83dc7c0cee62963e2f972e3e2ee3
Deepthi Sandeep <deepththampi@hotmail.com>
2024-11-09 18:17:33 -0600

    updated userRoutes

---
+++ src/routes/api/userRoutes.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} from '../../controllers/userControntroller';
+
+const router = Router();
+
+// /api/users
+router.route('/').get(getAllUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getUserById).put(updateUser).delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+
+export { router as userRouter };
