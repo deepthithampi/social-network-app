971085c4205a6844f0a146050804a6ffd06604b9
Deepthi Sandeep <deepththampi@hotmail.com>
2024-11-09 18:31:03 -0600

    index.ts and thoughtRoutes

--- src/routes/api/thoughtRoutes.ts
+++ src/routes/api/thoughtRoutes.ts
@@ -6,6 +6,22 @@ createThought,
 deleteThought,
 addReaction,
 removeReaction,
+updateThought,
 }from '../../controllers/thoughtController' ;
 
 const router = Router();
+
+// /api/thoughts
+router.route('/').get(getAllThoughts).post(createThought) ;
+
+// /api/thoughts/:toughtId
+router.route('/:thoughtId').get(getAllThoughts).put(updateThought).delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
+
+export {router as thoughtRouter}
+
