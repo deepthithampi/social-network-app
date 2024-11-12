a3ad8aa4023da8fded57aa2129df3fde4f6cbd78
Deepthi Sandeep <deepththampi@hotmail.com>
2024-11-11 22:35:03 -0600

    Updated seeds/index.ts 002

--- src/seeds/index.ts
+++ src/seeds/index.ts
@@ -1,10 +1,46 @@
 import db from '../config/connection.js';
 import {User, Thought} from '../models/index.js';
 import cleanDB from './cleanDB.js';
-import { getRandonUsername,getRandomEmail,getRandomThought,getRandomReactions } from './data.js';
+import { getRandonUsername,getRandomEmail,getRandomThought,getRandomReactions, getRandomArrItem } from './data.js';
 
 const seedDatabase = async () =>{
+ try{
+    // db connectivity  
+    await db();
 
+    // clean db
+    await cleanDB();
+
+    // arrays foe users and thoughts
+    const users = [];
+    const thoughts = [];
+    // generate 5 users with random username asn emails
+    for (let i=0;i<5;i++){
+        const username = getRandonUsername();
+        const email = getRandomEmail(username);
+
+        users.push({username,email});
+    }
+    //insert usrs into the db
+    const userData = await User.create(users);
+
+    //generate 10 thiughts , each linked to a random user
+    for (let i = 0;i<10;i++){
+        const thoughText = getRandomThought();
+        const username = getRandomArrItem(userData).username; //associating thought with a random username
+        const reactions = getRandomReactions(2); //each thought has 2 randim reactins
+         
+    }
+    //insert thoughts into the db
+
+    //update users to include thought references
+
+
+
+
+ }catch(e){
+
+ }
 };
 
 seedDatabase();
