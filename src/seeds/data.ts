18ea9ece3816c9785c249ceadc7b107a23664a41
Deepthi Sandeep <deepththampi@hotmail.com>
2024-11-11 20:51:57 -0600

    Updated data.ts 002

--- src/seeds/data.ts
+++ src/seeds/data.ts
@@ -14,4 +14,13 @@ const thoughTexts = [
 'I love learning in general especially coding',
 ];
 
+//get a random item from array
 export const getRandomArrItem = (arr: any[]) => arr[Math.floor(Math.random() * arr.length)];
+
+//generat random username
+export const getRandonUsername = () => getRandomArrItem(usernames);
+
+//generate a random emailId based on username
+export const getRandomEmail = (username : string) => `${username}@example.com`;
+
+//generate a random thought text
