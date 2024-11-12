8b472e2c7c4fd6c4c3a9aceb1730dc4c9b25b4be
Deepthi Sandeep <deepththampi@hotmail.com>
2024-11-11 21:07:33 -0600

    Updated seeds/ index.ts 001

---
+++ src/seeds/index.ts
@@ -0,0 +1,10 @@
+import db from '../config/connection.js';
+import {User, Thought} from '../models/index.js';
+import cleanDB from './cleanDB.js';
+import { getRandonUsername,getRandomEmail,getRandomThought,getRandomReactions } from './data.js';
+
+const seedDatabase = async () =>{
+
+};
+
+seedDatabase();
